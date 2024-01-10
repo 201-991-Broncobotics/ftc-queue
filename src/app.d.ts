f1757a7732237ad7439b2403eda0afbe70ffbc20
Rohan Godha <rgodha24@brophybroncos.org>
2024-01-10 10:20:12 -0700

    db

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,8 +6,12 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				DB: import('@cloudflare/workers-types').D1Database;
+			};
+		}
 	}
 }
 
-export {};
+export { };
