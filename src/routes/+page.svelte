d9156dfd924581be287dfb56fb7750c8f38cb8ec
Rohan Godha <rgodha24@brophybroncos.org>
2024-01-23 21:30:50 -0700

    tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,5 @@
 
 {JSON.stringify(data)}
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
